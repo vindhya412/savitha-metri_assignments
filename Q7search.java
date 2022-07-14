91e659eccaddf1fb98d6d36aefb13fe1cf1f9a6b
vindhya412 <107240769+vindhya412@users.noreply.github.com>
2022-07-14 14:42:15 +0530

    Create Q7search.java

---
+++ Q7search.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class search {
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int[] arr={5,12,14,6.78,1,23,26,35,37,7,52,86,47};
+    int query=s.nextInt();
+    for (int i=0;i<arr.length; i++) {
+      if(arr[i]==query){
+        System.out.println("your numberwas at idnex: "+i);
+        break;
+      }
+      else{
+      }
+    }
+  }
+}
