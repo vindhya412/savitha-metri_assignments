a4c5b2996c0a04b383a15fb6bcf7012c4ddf78d8
vindhya412 <107240769+vindhya412@users.noreply.github.com>
2022-07-14 11:54:46 +0530

    Create armstrong2.java

---
+++ core java/A1/armstrong2.java
@@ -0,0 +1,21 @@
+public Classs armstrong2 {
+  public Static void main(String[] args){
+    for(int n=100,n<1000;n++){
+      int sum=0;
+      int i=n;
+      while(i>0){
+        int dif=i%10;
+        sum=sum+dif*dif*dif;
+        i/=10;
+      }
+      if(n==sum){
+        System.out.println("the following is an armstrong number:+n);
+                           
+        }  
+             } 
+                 
+               }
+                  }
+                           
+                           
+                     
